88f9f96b11bf12fcd41ecd7d27fce34e3824faf8
Laura Brehm <laurabrehm@hey.com>
2024-10-17 11:09:23 +0100

    org mode and deps updates
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,171 +1,172 @@
 return {
-  -- code snippets for completion
-  {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-    dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end
-  },
-  { "hrsh7th/cmp-nvim-lsp", },
-  { "hrsh7th/cmp-path", },
-  { "hrsh7th/cmp-buffer", },
-  { "hrsh7th/cmp-cmdline" },
-  {
-    "petertriho/cmp-git",
-    dependencies = { 'hrsh7th/nvim-cmp' },
-  },
+    -- code snippets for completion
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
+    },
+    { "hrsh7th/cmp-nvim-lsp", },
+    { "hrsh7th/cmp-path", },
+    { "hrsh7th/cmp-buffer", },
+    { "hrsh7th/cmp-cmdline" },
+    {
+        "petertriho/cmp-git",
+        dependencies = { 'hrsh7th/nvim-cmp' },
+    },
 
-  -- code completion
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require("luasnip")
-      require("luasnip/loaders/from_vscode").lazy_load()
+    -- code completion
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require 'cmp'
+            local luasnip = require("luasnip")
+            require("luasnip/loaders/from_vscode").lazy_load()
 
-      local kind_icons = {
-        Text = "󰉿",
-        Method = "󰆧",
-        Function = "󰊕",
-        Constructor = "",
-        Field = " ",
-        Variable = "󰀫",
-        Class = "󰠱",
-        Interface = "",
-        Module = "",
-        Property = "󰜢",
-        Unit = "󰑭",
-        Value = "󰎠",
-        Enum = "",
-        Keyword = "󰌋",
-        Snippet = "",
-        Color = "󰏘",
-        File = "󰈙",
-        Reference = "",
-        Folder = "󰉋",
-        EnumMember = "",
-        Constant = "󰏿",
-        Struct = "",
-        Event = "",
-        Operator = "󰆕",
-        TypeParameter = " ",
-        Misc = " ",
-      }
+            local kind_icons = {
+                Text = "󰉿",
+                Method = "󰆧",
+                Function = "󰊕",
+                Constructor = "",
+                Field = " ",
+                Variable = "󰀫",
+                Class = "󰠱",
+                Interface = "",
+                Module = "",
+                Property = "󰜢",
+                Unit = "󰑭",
+                Value = "󰎠",
+                Enum = "",
+                Keyword = "󰌋",
+                Snippet = "",
+                Color = "󰏘",
+                File = "󰈙",
+                Reference = "",
+                Folder = "󰉋",
+                EnumMember = "",
+                Constant = "󰏿",
+                Struct = "",
+                Event = "",
+                Operator = "󰆕",
+                TypeParameter = " ",
+                Misc = " ",
+            }
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-          end,
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              if luasnip.expandable() then
-                luasnip.expand()
-              else
-                cmp.confirm({
-                  select = true,
-                })
-              end
-            else
-              fallback()
-            end
-          end),
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                    end,
+                },
+                window = {
+                    -- completion = cmp.config.window.bordered(),
+                    -- documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            if luasnip.expandable() then
+                                luasnip.expand()
+                            else
+                                cmp.confirm({
+                                    select = true,
+                                })
+                            end
+                        else
+                            fallback()
+                        end
+                    end),
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            -- Kind icons
-            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              luasnip = "[Snippet]",
-              buffer = "[Buffer]",
-              path = "[Path]",
-            })[entry.source.name]
-            return vim_item
-          end,
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
-          { name = 'buffer' },
-          { name = 'path' },
-          { name = 'git' },
-        }
-      })
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = function(entry, vim_item)
+                        -- Kind icons
+                        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+                        -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+                        vim_item.menu = ({
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[Snippet]",
+                            buffer = "[Buffer]",
+                            path = "[Path]",
+                        })[entry.source.name]
+                        return vim_item
+                    end,
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'git' },
+                    { name = 'orgmode' },
+                }
+            })
 
-      require("cmp_git").setup({
-        filetypes = { "*" },
-        trigger_actions = {
-          {
-            debug_name = "git_commits",
-            trigger_character = "@",
-            action = function(sources, trigger_char, callback, params, git_info)
-              return sources.git:get_commits(callback, params, trigger_char)
-            end,
-          },
-          {
-            debug_name = "github_issues_and_pr",
-            trigger_character = "#",
-            action = function(sources, trigger_char, callback, params, git_info)
-              return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
-            end,
-          },
-        },
-      })
+            require("cmp_git").setup({
+                filetypes = { "*" },
+                trigger_actions = {
+                    {
+                        debug_name = "git_commits",
+                        trigger_character = "@",
+                        action = function(sources, trigger_char, callback, params, git_info)
+                            return sources.git:get_commits(callback, params, trigger_char)
+                        end,
+                    },
+                    {
+                        debug_name = "github_issues_and_pr",
+                        trigger_character = "#",
+                        action = function(sources, trigger_char, callback, params, git_info)
+                            return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
+                        end,
+                    },
+                },
+            })
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
 
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        }),
-        matching = { disallow_symbol_nonprefix_matching = false }
-      })
-    end
-  },
+            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                }),
+                matching = { disallow_symbol_nonprefix_matching = false }
+            })
+        end
+    },
 
 }
---
+++ lua/plugins/org.lua
@@ -0,0 +1,125 @@
+return {
+    {
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            -- Setup orgmode
+            require('orgmode').setup({
+                org_agenda_files = '~/orgfiles/**/*',
+                org_default_notes_file = '~/orgfiles/refile.org',
+
+                org_todo_keywords = { 'TODO(t)', 'DOING(o@/!)', 'WAIT(w@/!)', '|', 'DONE(d)' },
+                mappings = {
+                    org_return_uses_meta_return = true
+                }
+
+                -- mappings = {
+                --     org_agenda_clock_in = "<leader>ci"
+                -- },
+                -- open in float
+                -- win_split_mode = function(name)
+                --     -- Make sure it's not a scratch buffer by passing false as 2nd argument
+                --     local bufnr = vim.api.nvim_create_buf(false, false)
+                --     --- Setting buffer name is required
+                --     vim.api.nvim_buf_set_name(bufnr, name)
+                --
+                --     local fill = 0.8
+                --     local width = math.floor((vim.o.columns * fill))
+                --     local height = math.floor((vim.o.lines * fill))
+                --     local row = math.floor((((vim.o.lines - height) / 2) - 1))
+                --     local col = math.floor(((vim.o.columns - width) / 2))
+                --
+                --     vim.api.nvim_open_win(bufnr, true, {
+                --         relative = "editor",
+                --         width = width,
+                --         height = height,
+                --         row = row,
+                --         col = col,
+                --         style = "minimal",
+                --         border = "rounded"
+                --     })
+                -- end
+            })
+
+            -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+            -- add ~org~ to ignore_install
+            -- require('nvim-treesitter.configs').setup({
+            --   ensure_installed = 'all',
+            --   ignore_install = { 'org' },
+            -- })
+        end,
+    },
+    {
+        'akinsho/org-bullets.nvim',
+        config = function()
+            require('org-bullets').setup()
+        end
+    },
+    {
+        "nvim-orgmode/telescope-orgmode.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-orgmode/orgmode",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("orgmode")
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'org',
+                group = vim.api.nvim_create_augroup('orgmode_telescope_nvim', { clear = true }),
+                callback = function()
+                    vim.keymap.set('n', '<leader>or', require('telescope').extensions.orgmode.refile_heading)
+                end,
+            })
+
+            vim.keymap.set("n", "<leader>r", require("telescope").extensions.orgmode.refile_heading)
+            vim.keymap.set("n", "<leader>fh", require("telescope").extensions.orgmode.search_headings)
+            vim.keymap.set("n", "<leader>li", require("telescope").extensions.orgmode.insert_link)
+        end,
+    },
+    {
+        "danilshvalov/org-modern.nvim",
+        dependencies = {
+            "nvim-orgmode/orgmode"
+        },
+        config = function()
+            local Menu = require("org-modern.menu")
+            require("orgmode").setup({
+                ui = {
+                    menu = {
+                        handler = function(data)
+                            Menu:new({
+                                window = {
+                                    margin = { 1, 0, 1, 0 },
+                                    padding = { 0, 1, 0, 1 },
+                                    title_pos = "center",
+                                    border = "single",
+                                    zindex = 1000,
+                                },
+                                icons = {
+                                    separator = "➜",
+                                },
+                            }):open(data)
+                        end,
+                    },
+                },
+            })
+        end
+    },
+    -- {
+    --     "chipsenkbeil/org-roam.nvim",
+    --     tag = "0.1.0",
+    --     dependencies = {
+    --         {
+    --             "nvim-orgmode/orgmode",
+    --             tag = "0.3.4",
+    --         },
+    --     },
+    --     config = function()
+    --         require("org-roam").setup({
+    --             directory = "~/orgfiles",
+    --         })
+    --     end
+    -- }
+}
