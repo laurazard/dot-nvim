fdea62b5a22e95bf729d265c187b5c54ed1f1afe
Laura Brehm <laurabrehm@hey.com>
2025-01-17 14:07:10 +0100

    cmp: remove blink.compat compatibility layer
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lua/plugins/code/completion.lua
+++ lua/plugins/code/completion.lua
@@ -1,15 +1,4 @@
 return {
-    -- blink nvim-cmp compatiility layer
-    {
-        'saghen/blink.compat',
-        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-        version = '*',
-        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-        lazy = true,
-        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-        opts = {},
-    },
-
     {
         "xzbdmw/colorful-menu.nvim",
         config = function()
