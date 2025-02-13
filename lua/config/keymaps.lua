7de413ca1be41b3bd70e53e21e8b710f9cddcdbf
Laura Brehm <laurabrehm@hey.com>
2025-02-13 14:47:26 +0100

    keymaps: re-add tmux keymaps
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,31 +4,36 @@ map = vim.keymap.set
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 
+map("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>")
+map("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>")
+map("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>")
+map("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>")
+
 -- custom (hide vendor/test) ignore patterns
 local telescope_ignore_patterns = {
-  {},
-  {
-    "[^a-z]vendor[^a-z]",
-    "vendor[^a-z]",
-  },
-  {
-    "[^a-z]test[^a-z]",
-    "Test[^a-z]",
-  },
-  {
-    "[^a-z]test[^a-z]",
-    "[^a-z]vendor[^a-z]",
-    "Test[^a-z]",
-    "vendor[^a-z]",
-  },
+    {},
+    {
+        "[^a-z]vendor[^a-z]",
+        "vendor[^a-z]",
+    },
+    {
+        "[^a-z]test[^a-z]",
+        "Test[^a-z]",
+    },
+    {
+        "[^a-z]test[^a-z]",
+        "[^a-z]vendor[^a-z]",
+        "Test[^a-z]",
+        "vendor[^a-z]",
+    },
 }
 map("n", "<C-v>", function()
-  local current_index = vim.g.telescope_ignore_index or 1
-  require("telescope.config").set_defaults({
-    file_ignore_patterns = telescope_ignore_patterns[current_index + 1],
-  })
-  local ignored_patterns = table.concat(telescope_ignore_patterns[current_index + 1], " ")
-  require("notify")("Toggle telescope ignore patterns: " .. ignored_patterns)
-  local next_index = ((current_index + 1) % 4)
-  vim.g.telescope_ignore_index = next_index
+    local current_index = vim.g.telescope_ignore_index or 1
+    require("telescope.config").set_defaults({
+        file_ignore_patterns = telescope_ignore_patterns[current_index + 1],
+    })
+    local ignored_patterns = table.concat(telescope_ignore_patterns[current_index + 1], " ")
+    require("notify")("Toggle telescope ignore patterns: " .. ignored_patterns)
+    local next_index = ((current_index + 1) % 4)
+    vim.g.telescope_ignore_index = next_index
 end, { desc = "Toggle telescope ignore patterns" })
