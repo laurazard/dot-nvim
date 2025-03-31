c6bcf89d3a742cd210b345a0b101c41ad7ae33f6
Laura Brehm <laurabrehm@hey.com>
2025-03-31 20:41:21 +0100

    lsp: fix yamlls config
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -16,13 +16,8 @@ return {
             -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
             url = "",
         },
+        yaml = {
+            schemas = require("schemastore").yaml.schemas()
+        },
     },
-    -- lazy-load schemastore when needed
-    on_new_config = function(new_config)
-        new_config.settings.yaml.schemas = vim.tbl_deep_extend(
-            "force",
-            new_config.settings.yaml.schemas or {},
-            require("schemastore").yaml.schemas()
-        )
-    end,
 }
