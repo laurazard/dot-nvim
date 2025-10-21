1483bbff4fdfa4372b0f3ff7e270f4f543d1a10b
Laura Brehm <laurabrehm@hey.com>
2025-10-21 11:33:22 +0200

    ui: make neotree sidebar wider
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lua/plugins/editor/ui.lua
+++ lua/plugins/editor/ui.lua
@@ -262,7 +262,7 @@ return {
                         filter = function(buf)
                             return vim.b[buf].neo_tree_source == "filesystem"
                         end,
-                        size = { height = 0.5 },
+                        size = { height = 0.5, width = .25 },
                     },
                     {
                         title = "Harpoon Buffers",
