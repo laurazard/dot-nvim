124506e4acf56deffb836be686a84280955b8d0f
Laura Brehm <laurabrehm@hey.com>
2025-11-04 16:59:50 +0100

    fix: disable text in golangci-lint command line args
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lsp/golangci_lint_ls.lua
+++ lsp/golangci_lint_ls.lua
@@ -3,6 +3,11 @@ return {
     cmd = { 'golangci-lint-langserver' },
     filetypes = { 'go', 'gomod' },
     init_options = {
-        command = { 'golangci-lint', 'run', '--out-format=json', '--show-stats=false' },
+        command = { 'golangci-lint', 'run',
+            '--output.json.path=stdout',
+            '--output.text.path=stderr',
+            '--output.text.print-linter-name=false',
+            '--output.text.print-issued-lines=false',
+            '--show-stats=false', '--issues-exit-code=1' },
     }
 }
