68ccc8dfb06bb90c0e913cc9bf7fee962231f4c9
Laura Brehm <laurabrehm@hey.com>
2025-11-20 13:56:27 +0100

    fix ccls, etc.
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,5 +1,8 @@
 ---@type vim.lsp.Config
 return {
+    -- mason adds `vim.fn.stdpath("data") .. "/mason/bin/"` to VIM's PATH
+    -- hence, packages installed by mason will be found just by name, i.e.
+    -- basedpyright-langserver
     cmd = { 'basedpyright-langserver', '--stdio' },
     filetypes = { 'python' },
     single_file_support = true,
--- lua/plugins/lang/c.lua
+++ lua/plugins/lang/c.lua
@@ -19,20 +19,15 @@ return {
             opts = opts or {}
             opts.lsp = opts.lsp or {}
 
-            local util = require "lspconfig.util"
             local server_config = {
                 filetypes = { "c", "cpp", "objc", "objcpp", "opencl" },
-                root_dir = function(fname)
-                    return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
-                        or util.find_git_ancestor(fname)
-                end,
                 init_options = {
                     cache = {
                         directory = vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
                     }
                 },
             }
-            opts.lsp.lspconfig = server_config
+            opts.lsp.server = server_config
             require("ccls").setup(opts)
         end
     },
